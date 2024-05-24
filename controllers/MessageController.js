258471d420b80dbdc7cd562a32c4fd71fe9551a1
farukulWD <farukgb1999@gmail.com>
2024-05-25 05:19:38 +0600

    message get and socket connection done

---
+++ controllers/MessageController.js
@@ -0,0 +1,77 @@
+import getPrismaInstance from "../utils/PrismaClient.js";
+
+export const addMessage = async (req, res, next) => {
+  try {
+    const { to, from, message } = req.body;
+    const getUser = onlineUsers.get(to);
+    if (message && from && to) {
+      const Prisma = getPrismaInstance();
+      const newMessage = await Prisma.messages.create({
+        data: {
+          message,
+          sender: { connect: { id: parseInt(from) } },
+          receiver: { connect: { id: parseInt(to) } },
+          messageStatus: getUser ? "delivered" : "send",
+        },
+        include: { sender: true, receiver: true },
+      });
+      return res.status(201).send({ message: newMessage });
+    }
+    return res.status(400).send("From,to and Message are required");
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  const { from, to } = req.params;
+
+  try {
+    const Prisma = getPrismaInstance();
+    const messages = await Prisma.messages.findMany({
+      where: {
+        OR: [
+          {
+            senderId: parseInt(from),
+            receiverId: parseInt(to),
+          },
+          {
+            senderId: parseInt(to),
+            receiverId: parseInt(from),
+          },
+        ],
+      },
+      orderBy: {
+        id: "asc",
+      },
+      include: { sender: true, receiver: true },
+    });
+
+    const unreadMessages = [];
+
+    messages.forEach((message) => {
+      if (
+        message.messageStatus !== "read" &&
+        message.senderId === parseInt(to)
+      ) {
+        message.messageStatus = "read";
+        unreadMessages.push(message.id);
+      }
+    });
+
+    if (unreadMessages.length > 0) {
+      await Prisma.messages.updateMany({
+        where: {
+          id: { in: unreadMessages },
+        },
+        data: {
+          messageStatus: "read",
+        },
+      });
+    }
+    res.status(200).json({ messages });
+  } catch (error) {
+    next(error);
+  }
+};
--- index.js
+++ index.js
@@ -2,15 +2,41 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import AuthRoutes from "./routes/AuthRoutes.js";
+import MessageRoutes from "./routes/MessageRoutes.js";
+import { Server } from "socket.io";
 
 dotenv.config();
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", AuthRoutes);
+app.use("/api/messages", MessageRoutes);
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId) => {
+    console.log(userId);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", {
+        from: data.from,
+        message: data.message,
+        to: data.to,
+      });
+    }
+  });
 });
